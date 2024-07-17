63d99e345945fd16a8969a64168c442cb1c9e652
Harica404 <68884486+ericljx2020-gmail@users.noreply.github.com>
2024-07-17 11:42:33 +0800

    construct fucntions

--- similar_score/functions.py
+++ similar_score/functions.py
@@ -8,6 +8,9 @@ import nest_asyncio
 import asyncio
 import re
 from typing import Any, Dict
+from templates import extract_prompt
+import torch
+from langchain_openai import OpenAIEmbeddings
 
 
 '''
@@ -36,3 +39,43 @@ async def inference(params: Dict[str, any], template: str, max_tokens=1000) -> s
         print(chunk, end='', flush=True)
 
     return output
+
+
+#function 输入两题，返回两题的cos similarity
+async def get_similarity(p1: str, p2: str) -> float:
+    tot = 0
+    min_old = 3
+    max_old = 10
+    min_new = 1
+    max_new = 10
+    weights = [0.4, 0.2, 0.15, 0.05, 0.05, 0.15]
+    extc_p1 = []
+    extc_p2 = []
+    att_p1 = await inference({"problem": p1}, extract_prompt)
+    att_p2 = await inference({"problem": p2}, extract_prompt)
+    pattern = r'\d+\.\s.*?:\s*(.*)'
+    matches_p1 = re.findall(pattern, att_p1)
+    matches_p2 = re.findall(pattern, att_p2)
+    for i, match in enumerate(matches_p1, 1):
+        extc_p1.append(match)
+    for i, match in enumerate(matches_p2, 1):
+        extc_p2.append(match)
+    for i in range(6):
+        tot += weights[i] * cosimilarity(extc_p1[i], extc_p2[i])
+    return ((tot * 10 - min_old) / (max_old - min_old)) * (max_new - min_new) + min_new
+
+
+text2embed = {}
+def cosimilarity(p1: str, p2: str):
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+    if p1 in text2embed: 
+        p1_emb = text2embed.get(p1)
+    else: 
+        p1_emb = torch.tensor(embedding_model.embed_query(p1))
+        text2embed[p1] = p1_emb
+    if p2 in text2embed:
+        p2_emb = text2embed.get(p2)
+    else:
+        p2_emb = torch.tensor(embedding_model.embed_query(p2))
+        text2embed[p2] = p2_emb
+    return torch.dot(p1_emb, p2_emb) / (torch.norm(p1_emb) * torch.norm(p2_emb))
